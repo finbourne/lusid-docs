ea55683ae151aafb3ca5ee0c7e1b083d4e734a3c
Mark Needham <mark.needham@finbourne.com>
2021-06-01 16:15:42 +0100

    ibor

--- scripts/download-scripts.js
+++ scripts/download-scripts.js
@@ -123,6 +123,8 @@ const files = [
         "modules/ROOT/examples/ibor/transactions.csv"],
     [`${python_tools_base}/data/test_ibor/test_output/transactions_response.csv`,
         "modules/ROOT/examples/ibor/transactions_response.csv"],
+    [`${python_tools_base}/data/test_ibor/test_output/transactions_response_all.csv`,
+        "modules/ROOT/examples/ibor/transactions_response_all.csv"],
     [`${python_tools_base}/data/test_ibor/test_output/transactions_filter_response.csv`,
         "modules/ROOT/examples/ibor/transactions_filter_response.csv"],
 
